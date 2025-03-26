d32671eaf5f80eda2d71f4fb7cf673e2560da0cf
Bruno Dornelas <bruno.alves.dornelas@gmail.com>
2025-03-26 02:10:33 -0300

    Adicionando dados da base de dados

---
+++ src/database/database.js
@@ -0,0 +1,30 @@
+// Importa o módulo mysql2/promise, que é usado para conectar ao banco de dados MySQL usando promises.
+const mysql = require("mysql2/promise");
+
+// Cria um pool de conexões com o banco de dados MySQL.
+const pool = mysql.createPool({
+  // Define o host do banco de dados.
+  host: process.env.host,
+
+  // Define o nome de usuário para conectar ao banco de dados.
+  user: process.env.user,
+
+  // Define a senha para conectar ao banco de dados.
+  password: process.env.password,
+
+  // Define o nome do banco de dados a ser usado.
+  database: process.env.database,
+
+  // Define se o pool deve esperar por conexões disponíveis se todas as conexões estiverem em uso.
+  waitForConnections: true,
+
+  // Define o número máximo de conexões que o pool pode manter.
+  connectionLimit: 10,
+
+  // Define o número máximo de requisições que podem ser enfileiradas se todas as conexões estiverem em uso.
+  // 0 significa que não há limite.
+  queueLimit: 0,
+});
+
+// Exporta o pool de conexões para ser usado em outros módulos.
+module.exports = pool;
