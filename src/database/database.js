615e077f473781ac598cbb9084631c168725430b
Bruno Dornelas <bruno.alves.dornelas@gmail.com>
2025-03-26 03:12:21 -0300

    Arrumando variaveis heroku

--- src/database/database.js
+++ src/database/database.js
@@ -4,16 +4,16 @@ const mysql = require("mysql2/promise");
 // Cria um pool de conexões com o banco de dados MySQL.
 const pool = mysql.createPool({
   // Define o host do banco de dados.
-  host: process.env.host,
+  host: process.env.host_db,
 
   // Define o nome de usuário para conectar ao banco de dados.
-  user: process.env.user,
+  user: process.env.user_db,
 
   // Define a senha para conectar ao banco de dados.
-  password: process.env.password,
+  password: process.env.password_db,
 
   // Define o nome do banco de dados a ser usado.
-  database: process.env.database,
+  database: process.env.database_db,
 
   // Define se o pool deve esperar por conexões disponíveis se todas as conexões estiverem em uso.
   waitForConnections: true,
